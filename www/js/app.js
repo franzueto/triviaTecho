9ea8ca074f14a4bd9ed47caf85e6e2d5a5365510
Francisco Anzueto <franzueto@gmail.com>
2015-06-19 23:45:10 -0600

    Facebook Login and home with left menu

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,36 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB'])
 
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+  .state('index', {
+    url: '/index',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/test.html'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/index');
+})
+
 .run(function($ionicPlatform, $state, $rootScope, ngFB) {
+  ngFB.init({appId: '449892315192982'});
+
+  Parse.initialize("tkMF7B1eoP2uRDfuQPmGm5zXXnxUa4YxPjKRlCbg", "opJpMr5OodNaMCzY86uX66LprUzNRzJt9Wuys7Oh");
+
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,35 +44,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngOpenFB'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    //Parse.User.logOut();
+    //$rootScope.user = null;
+    //$rootScope.isLoggedIn = false;
+
+    var currentUser = Parse.User.current();
+    $rootScope.user = null;
+    $rootScope.isLoggedIn = false;
+
+    if (currentUser) {
+      $rootScope.user = currentUser;
+      $rootScope.isLoggedIn = true;
+      $state.go('app.home');
+    }
   });
-
-  ngFB.init({appId: '449892315192982'});
-  /*
-  Parse.initialize("tkMF7B1eoP2uRDfuQPmGm5zXXnxUa4YxPjKRlCbg", "opJpMr5OodNaMCzY86uX66LprUzNRzJt9Wuys7Oh");
-  var currentUser = Parse.User.current();
-  $rootScope.user = null;
-  $rootScope.isLoggedIn = false;
-
-  if (currentUser) {
-    $rootScope.user = currentUser;
-    $rootScope.isLoggedIn = true;
-    $state.go('test');
-  }*/
-
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('index', {
-    url: '/',
-    templateUrl: 'templates/login.html',
-    controller: 'LoginCtrl'
-  })
-  .state('test', {
-    url: '/test',
-    templateUrl: 'templates/test.html'
-  })
-  ;
-
-  $urlRouterProvider.otherwise('/');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,85 @@
 angular.module('starter.controllers', ['ngOpenFB'])
 
-.controller('LoginCtrl', function($scope, $ionicModal, $timeout, ngFB) {
+.controller('AppCtrl', function($scope, $state, $rootScope, $ionicHistory, $stateParams) {
+  if ($stateParams.clear) {
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+  }
+
+  $scope.logout = function() {
+    Parse.User.logOut();
+    $rootScope.user = null;
+    $rootScope.isLoggedIn = false;
+    $state.go('index', {
+      clear: true
+    });
+  };
+})
+
+.controller('LoginCtrl', function($scope, $rootScope, $state, $ionicLoading, $ionicHistory, $stateParams, ngFB) {
+  if ($stateParams.clear) {
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearCache();
+  }
+
   $scope.fbLogin = function(){
+    $scope.loading = $ionicLoading.show({
+      content: 'Logging in',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
+    });
+
     ngFB.login().then(
       function (response) {
         if (response.status === 'connected') {
-          alert('Facebook login succeeded');
           console.log('Facebook login succeeded');
-          //$scope.closeLogin();
+
+          ngFB.api({
+            path: '/me',
+            params: {fields: 'id,name'}
+          }).then(
+            function (user) {
+                $scope.user = user;
+
+                Parse.User.logIn(user.id, user.id, {
+                  success: function(user_logged) {
+                    // Do stuff after successful login.
+                    $rootScope.user = user_logged;
+                    $rootScope.isLoggedIn = true;
+                    $ionicLoading.hide();
+                    $state.go('app.home');
+                  },
+                  error: function(user_logged, error) {
+                    var user_parse = new Parse.User();
+                    user_parse.set("username", user.id);
+                    user_parse.set("password", user.id);
+
+                    user_parse.signUp(null, {
+                      success: function(user_parse_signed) {
+                        console.log(user_parse_signed);
+                        $rootScope.user = user_parse_signed;
+                        $rootScope.isLoggedIn = true;
+                        $ionicLoading.hide();
+                        $state.go('app.home');
+                      },
+                      error: function(user_parse_signed, error) {
+                        // Show the error message somewhere and let the user try again.
+                        $ionicLoading.hide();
+                        alert("Error: " + error.code + " " + error.message);
+                      }
+                    });
+                  }
+                });
+            },
+            function (error) {
+              $ionicLoading.hide();
+              alert('Facebook error: ' + error.error_description);
+            });
         } else {
           alert('Facebook login failed');
+          $ionicLoading.hide();
         }
       });
   }
